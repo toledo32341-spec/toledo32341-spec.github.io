3ec004cc70a6d3ee39eb52fcfbd0c35bfe697c41
toledo32341-spec <toledo32341@students.apsb.org>
2026-03-06 08:27:02 -0600

    Update platformer.js

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,36 +27,17 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-     toggleGrid();
+     //toggleGrid();
 
 
     // TODO 2 - Create Platforms
-createPlatform(350, 735, 200, 20, "hotpink"); // hotpink for the finished platform
-  createPlatform(700, 735, 200, 20, "purple"); // pruple for the finished platform 
-    createPlatform(700, 735, 200, 20, "red"); // red for the finished platform
-      createPlatform(650, 500, 200, 20, "blue"); // blue for the finished platform 
-        createPlatform(950, 500, 200, 20, "green"); // green for the finished platform 
-          createPlatform(350, 350, 200, 20, "orange"); // orange for the finished platform 
-            createPlatform(700, 350, 200, 20, "yellow"); // yellow for the finished platform 
-              createPlatform(1050, 350, 200, 20, "cyan"); // cyan for the finished platform 
 
 
     // TODO 3 - Create Collectables
-createCollectables("database", 335, 550, 0.5, 0.7);  
-  createCollectables("diamond", 950, 550, 0.5, 0.7); 
-    creatCollectables("grace", 630, 700, 0.5, 0.7); 
-      createCollectables("kennedi", 300, 550, 0.5, 0.7);
-        createCollectables("steve", 650, 550, 0.5, 0.7);  
 
 
     
     // TODO 4 - Create Cannons
-createCannon("left", 0, 450,);   
-  createCannon("left", 0, 225,);
-    createCannon("top", 850, 0); 
-      createCannon("top", 550, 0); 
-        createCannon("right", 1400, 225); 
-          createCannon("right", 1400, 450,); 
     
     
     //////////////////////////////////
